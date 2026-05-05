d073bdf0f81ec77c3f7c9c0e923cb419b502a2cb
elysisabb <isabelalopes.pf022@academico.ifsul.edu.br>
2026-05-05 14:19:13 -0300

    vetor

--- SCRIPT/q1_Arthur_Isa.js
+++ SCRIPT/q1_Arthur_Isa.js
@@ -1,5 +1,15 @@
-function executar() {
+let vetor = [];
+
+let n = parseInt(getElementById("nText"));
+
+/*function executar() {
     let valor = document.getElementById("campo").value;
     // processar...
     document.getElementById("saida").innerHTML = resultado;
+}*/
+
+function solucao(n) {
+    for(i=1; i<= n; i++){
+        vetor.push
+    }
 }
