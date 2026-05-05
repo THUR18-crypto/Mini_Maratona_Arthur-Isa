50616243695c2c640e9a3c499564861a9d927266
elysisabb <isabelalopes.pf022@academico.ifsul.edu.br>
2026-05-05 14:39:43 -0300

    solucao

--- SCRIPT/q1_Arthur_Isa.js
+++ SCRIPT/q1_Arthur_Isa.js
@@ -10,6 +10,21 @@ let n = parseInt(getElementById("nText"));
 
 function solucao(n) {
     for(i=1; i<= n; i++){
-        vetor.push
+        vetor.push(i);
+    }
+
+    for(i=0; i<n; i++){
+        if(vetor[i] % 3 == 0){
+           vetor[i] = "Fizz"; 
+        } 
+        else if(vetor[i] % 5 == 0){
+            vetor[i] = "Buzz";
+        } 
+        else if(vetor[i] % 7 == 0){
+            vetor[i] = "Boom";
+        }
+        else if(vetor[i] % 3 == 0 && vetor[i] % 5 == 0){
+            vetor[i] = "FizzBuzz";
+        }
     }
 }
