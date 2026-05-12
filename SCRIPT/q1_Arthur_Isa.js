f626cf6b8e83e7247c9c5fd2f7009648c8a19641
Arthur O.S <thurbrando18@gmail.com>
2026-05-12 15:43:35 -0300

    um pouco da questão 3

--- SCRIPT/q1_Arthur_Isa.js
+++ SCRIPT/q1_Arthur_Isa.js
@@ -3,24 +3,38 @@ function solucao() {
 
     let n = parseInt(document.getElementById("nText").value);
 
-    for(i=1; i<= n; i++){
+    for (i = 1; i <= n; i++) {
         vetor.push(i);
     }
 
-    for(i=0; i<n; i++){
-        if(vetor[i] % 3 == 0 && vetor[i] % 5 == 0){
+    for (i = 0; i < n; i++) {
+        if (vetor[i] % 3 == 0 && vetor[i] % 5 == 0) {
             vetor[i] = "FizzBuzz";
         }
-        else if(vetor[i] % 3 == 0){
-           vetor[i] = "Fizz"; 
-        } 
-        else if(vetor[i] % 5 == 0){
+        else if (vetor[i] % 3 == 0) {
+            vetor[i] = "Fizz";
+        }
+        else if (vetor[i] % 5 == 0) {
             vetor[i] = "Buzz";
-        } 
-        else if(vetor[i] % 7 == 0){
+        }
+        else if (vetor[i] % 7 == 0) {
             vetor[i] = "Boom";
-        } 
+        }
     }
 
     alert(`Resposta: ${vetor}`);
 }
+function solucaoQ3() {
+    let vetor = ["p","a","l"]
+    
+
+    for (let index = 0; index < vetor.length; index++) {
+        vetor[index] = vetor[vetor.length - index]
+        console.log(vetor[index])
+    }
+    console.log(vetor)
+}
+
+
+
+
